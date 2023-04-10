287df064e5924bc86a88833edb28c1881272deb3
zhisui <1626676263@qq.com>
2023-04-10 10:19:34 +0800

    feat:修改配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,27 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
     },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@import "@/assets/style/main.scss";',
-      },
+    server: {
+        host: '0.0.0.0',
+        proxy: {
+            '/api': {
+                target: 'http://172.18.21.6:8088', //服务器地址
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+        },
     },
-  },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: '@import "@/assets/style/main.scss";',
+            },
+        },
+    },
 })
