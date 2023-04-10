8f8908ba2c2d90957e20955ea83e87f2c3368e27
longy <longy@sucsoft.com>
2023-04-10 10:15:37 +0800

    feat:修改接口调用配置

--- src/service/api/login/login.ts
+++ src/service/api/login/login.ts
@@ -1,17 +1,17 @@
 import http from '@/service/http'
 
 export interface ILoginParams {
-  userName: string
-  passWord: string | number
+    userName: string
+    passWord: string | number
 }
 export interface ILoginApi {
-  login: (params: ILoginParams) => Promise<any>
+    login: (params: ILoginParams) => Promise<any>
 }
 //像这种类型定义不是很多的，我还是比较喜欢写在一个文件里面
 
 const loginApi: ILoginApi = {
-  login(params) {
-    return http.post('/login', params)
-  },
+    login(params) {
+        return http.post('/login', params)
+    },
 }
 export default loginApi
