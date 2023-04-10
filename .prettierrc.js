16f1b13b0f534c04bd9abd5c652be666a1bb438d
longy <longy@sucsoft.com>
2023-04-10 10:13:54 +0800

    feat:修改接口调用配置

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,17 +1,17 @@
 module.exports = {
-  tabWidth: 2,
-  jsxSingleQuote: true,
-  jsxBracketSameLine: true,
-  printWidth: 100,
-  singleQuote: true,
-  semi: false,
-  overrides: [
-    {
-      files: '*.json',
-      options: {
-        printWidth: 200,
-      },
-    },
-  ],
-  arrowParens: 'always',
+    tabWidth: 4,
+    jsxSingleQuote: true,
+    jsxBracketSameLine: true,
+    printWidth: 100,
+    singleQuote: true,
+    semi: false,
+    overrides: [
+        {
+            files: '*.json',
+            options: {
+                printWidth: 200,
+            },
+        },
+    ],
+    arrowParens: 'always',
 }
--- src/service/http.ts
+++ src/service/http.ts
@@ -2,105 +2,121 @@ import axios, { AxiosRequestConfig } from 'axios'
 import NProgress from 'nprogress'
 
 //设置请求头和请求路径
-axios.default.baseURL = '/api'
-axios.default.timeout = 10000
-axios.default.headers.post['Content-Type'] = 'application/json;charset=UTF-8'
+const baseURL = String(import.meta.env.VITE_APP_BASE)
+axios.defaults.baseURL = baseURL
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset=UTF-8'
 //请求拦截
 axios.interceptors.request.use(
-  (config): AxiosRequestConfig<any> => {
-    const token = window.sessionStorage.getItem('token')
-    if (token) {
-      config.headers.toke = token
+    (config): AxiosRequestConfig<any> => {
+        return config
+    },
+    (error) => {
+        return error
     }
-    return config
-  },
-  (error) => {
-    return error
-  }
 )
 //响应拦截
 axios.interceptors.response.use((res) => {
-  if (res.code === 111) {
-    sessionStorage.setItem('token', '')
-  }
-  //后面可以加很多种情况
-  return res
+    //后面可以加很多种情况
+    return res
 })
 
 interface ResType<T> {
-  code: number
-  data?: T
-  msg: string
-  error?: string
+    code: number
+    result?: T
+    message: string
 }
 
 interface Http {
-  get<T>(url: string, params?: unknow): Promise<ResType<T>>
-  post<T>(url: string, params?: unknow): Promise<ResType<T>>
-  upload<T>(url: string, params?: unknow): Promise<ResType<T>>
-  download<T>(url: string, params?: unknow): Promise<ResType<T>>
+    get<T>(url: string, params?: unknown): Promise<ResType<T>>
+    post<T>(url: string, params?: unknown): Promise<ResType<T>>
+    put<T>(url: string, params?: unknown): Promise<ResType<T>>
+    upload<T>(url: string, params?: unknown): Promise<ResType<T>>
+    download<T>(url: string, params?: unknown): void
 }
 
 const http: Http = {
-  get(url, params) {
-    return new Promise((resolve, reject) => {
-      NProgress.start()
-      axios
-        .get(url, { params })
-        .then((res) => {
-          NProgress.done()
-          resolve(res.data)
+    get(url, params) {
+        return new Promise((resolve, reject) => {
+            NProgress.start()
+            axios
+                .get(url, {
+                    params,
+                    // headers: {
+                    //     Range: 'bytes=0-1200',
+                    // },
+                })
+                .then((res) => {
+                    NProgress.done()
+                    resolve(res.data)
+                })
+                .catch((error) => {
+                    NProgress.done()
+                    reject(error.data)
+                })
         })
-        .catch((error) => {
-          NProgress.done()
-          reject(error.data)
+    },
+    // 这个是普通的post
+    post(url, params) {
+        return new Promise((resolve, reject) => {
+            NProgress.start()
+            axios
+                .post(url, JSON.stringify(params))
+                .then((res) => {
+                    NProgress.done()
+                    resolve(res.data)
+                })
+                .catch((err) => {
+                    NProgress.done()
+                    reject(err.data)
+                })
         })
-    })
-  },
+    },
+    // 上传文件的post
+    upload(url, file) {
+        return new Promise((resolve, reject) => {
+            NProgress.start()
+            axios
+                .post(url, file, {
+                    headers: { 'Content-Type': 'multipart/form-data' },
+                })
+                .then((res) => {
+                    NProgress.done()
+                    resolve(res.data)
+                })
+                .catch((err) => {
+                    NProgress.done()
+                    reject(err.data)
+                })
+        })
+    },
 
-  post(url, params) {
-    return new Promise((resolve, reject) => {
-      NProgress.start()
-      axios
-        .post(url, JSON.stringify(params))
-        .then((res) => {
-          NProgress.done()
-          resolve(res.data)
+    put(url, params) {
+        return new Promise((resolve, reject) => {
+            NProgress.start()
+            axios
+                .put(url, params, {
+                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                })
+                .then((res) => {
+                    NProgress.done()
+                    resolve(res.data)
+                })
+                .catch((err) => {
+                    NProgress.done()
+                    reject(err.data)
+                })
         })
-        .catch((err) => {
-          NProgress.done()
-          reject(err.data)
-        })
-    })
-  },
+    },
 
-  upload(url, file) {
-    return new Promise((resolve, reject) => {
-      NProgress.start()
-      axios
-        .post(url, file, {
-          headers: { 'Content-Type': 'multipart/form-data' },
-        })
-        .then((res) => {
-          NProgress.done()
-          resolve(res.data)
-        })
-        .catch((err) => {
-          NProgress.done()
-          reject(err.data)
-        })
-    })
-  },
-
-  download(url) {
-    const iframe = document.createElement('iframe')
-    iframe.style.display = 'none'
-    iframe.src = url
-    iframe.onload = function () {
-      document.body.removeChild(iframe)
-    }
-    document.body.appendChild(iframe)
-  },
+    download(url) {
+        const iframe = document.createElement('iframe')
+        iframe.style.display = 'none'
+        iframe.src = url
+        iframe.onload = function () {
+            document.body.removeChild(iframe)
+        }
+        document.body.appendChild(iframe)
+    },
 }
 
 export default http
